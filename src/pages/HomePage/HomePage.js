8b689b6caf9a11ba7e33674516212e385d452ffb
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-01-24 13:46:55 -0800

    add boiler code

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    // Fetching Movies: https://api.themoviedb.org/3/discover/movie/?api_key=a05e7d6beb2f7a3517a89a11bbe7d381
+    // Genre id to fetch Genre: https://api.themoviedb.org/3/genre/movie/list?api_key=a05e7d6beb2f7a3517a89a11bbe7d381
+    
+    <div>
+      <h1>Movies List</h1>
+    </div>
+  )
+}
+
+export default HomePage
